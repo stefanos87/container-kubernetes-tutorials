4ee7b5c66dcb10b8413d25a548de7d95548c7164
Roberto Pozzi <r.robipozzi@gmail.com>
2019-06-27 09:38:06 +0200

    updates

--- 4-docker_network/README.md
+++ 4-docker_network/README.md
@@ -1,1 +1,2 @@
 # Docker networking tutorial
+[TODO]
--- 4-docker_network/rpozzi-restaurants-ui/docker-run.sh
+++ 4-docker_network/rpozzi-restaurants-ui/docker-run.sh
@@ -5,4 +5,6 @@ docker rm -f $DOCKER_CONTAINER_NAME
 echo ${cyn}Docker container removed${end}
 echo
 echo ${cyn}Running Docker container ...${end}
-docker run -it -p $DOCKER_CONTAINER_PORT:80 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
+DOCKER_CMD_RUN="docker run -it --name $DOCKER_CONTAINER_NAME -p $DOCKER_CONTAINER_PORT:80 $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION"
+echo ${cyn}Running with:${end} ${grn}$DOCKER_CMD_RUN${end}
+$DOCKER_CMD_RUN
