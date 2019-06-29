9e27e6793377075a48a57d5726dea200bf4713d1
Roberto Pozzi <r.robipozzi@gmail.com>
2019-06-29 09:28:37 +0200

    updates

--- 4-docker_network/rpozzi-restaurants-backend/app/services/restaurantService.js
+++ 4-docker_network/rpozzi-restaurants-backend/app/services/restaurantService.js
@@ -6,18 +6,19 @@ var propertiesReader = require('properties-reader');
 
 function getRestaurants(callback) {
 	logger.info("RestaurantService.getRestaurants called");
-	var restaurants = __getFakeRestaurants();
-	callback({ restaurants : restaurants});
+	// Read properties from a file
+	// Refer to: https://www.npmjs.com/package/properties-reader
+	__getFakeRestaurants(callback);
 }
 
-function __getFakeRestaurants() {
+function __getFakeRestaurants(callback) {
 	var restaurant1 = { id : 1, name : "Hostaria Vecchio Portico", city : "Arona", rating: 4};
-	var restaurant2 = { id : 2, name : "Il Cugino di Campagna", city : "Gallarate", rating: 4}
+	var restaurant2 = { id : 2, name : "Il Ragazzo di Campagna", city : "Gallarate", rating: 4}
 	var restaurant3 = { id : 3, name : "Il Cormorano", city : "Milano", rating: 3}
 	var restaurant4 = { id : 4, name : "Roadhouse", city : "Varese", rating: 3}
 	var restaurant5 = { id : 5, name : "Arancioamaro", city : "Cannero", rating: 3}
 	var restaurants = [restaurant1, restaurant2, restaurant3, restaurant4, restaurant5];
-	return restaurants;
+	callback({ restaurants : restaurants});
 }	
 
 exports.getRestaurants = getRestaurants;
