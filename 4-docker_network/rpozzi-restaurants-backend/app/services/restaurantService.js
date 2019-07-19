28c5b17da01f715773a7b84a1d5e60fcd815ba86
Roberto Pozzi <r.robipozzi@gmail.com>
2019-07-19 21:46:19 +0200

    refactor

--- 4-docker_network/rpozzi-restaurants-backend/app/services/restaurantService.js
+++ 4-docker_network/rpozzi-restaurants-backend/app/services/restaurantService.js
@@ -3,32 +3,32 @@ var propertyReader = require('../utils/propertyReader');
 var logger = require('../utils/logger');
 // Functions
 function getRestaurants(callback) {
-	logger.info("RestaurantService.getRestaurants called");
-	var restaurants;
+	logger.debug("RestaurantService.getRestaurants called");
 	try {
 		var useDb = propertyReader.getProperty('use.db');
 		if (useDb) {
-			logger.info("db.type = " + propertyReader.getProperty('db.type'));
-			logger.info("db.url = " + propertyReader.getProperty('db.url'));
+			logger.debug("Getting restaurants from database");
+			logger.debug("db.type = " + propertyReader.getProperty('db.type'));
+			logger.debug("db.url = " + propertyReader.getProperty('db.url'));
+			logger.debug("db.port = " + propertyReader.getProperty('db.port'));
+			logger.debug("db.user = " + propertyReader.getProperty('db.user'));
 		} else {
 			logger.debug("Getting restaurants from stub service");
-			restaurants = __getFakeRestaurants();
+			__getFakeRestaurants(callback);
 		}
 	} catch (error) {
 		logger.error("Not able to read 'use.db' property, getting restaurants from stub service");
 		restaurants = __getFakeRestaurants();
 	}
-	callback({ restaurants : restaurants});
 }
-
-function __getFakeRestaurants() {
+function __getFakeRestaurants(callback) {
 	var restaurant1 = { id : 1, name : "Hostaria Vecchio Portico", city : "Arona", rating: 4};
 	var restaurant2 = { id : 2, name : "Il Ragazzo di Campagna", city : "Gallarate", rating: 4}
 	var restaurant3 = { id : 3, name : "Il Cormorano", city : "Milano", rating: 3}
 	var restaurant4 = { id : 4, name : "Roadhouse", city : "Varese", rating: 3}
 	var restaurant5 = { id : 5, name : "Arancioamaro", city : "Cannero", rating: 3}
 	var restaurants = [restaurant1, restaurant2, restaurant3, restaurant4, restaurant5];
-	return restaurants;
+	callback({ restaurants : restaurants});
 }
 
 exports.getRestaurants = getRestaurants;
