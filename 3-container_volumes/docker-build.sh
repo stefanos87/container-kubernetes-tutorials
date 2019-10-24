b245e8ceb1197e96ae586131dbd7a9ca354a4936
Roberto Pozzi <r.robipozzi@gmail.com>
2019-10-24 21:14:54 +0200

    fix docker build with --no-cache option

--- 3-container_volumes/docker-build.sh
+++ 3-container_volumes/docker-build.sh
@@ -4,7 +4,7 @@ docker rmi -f $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION
 echo ${cyn}Docker image removed${end}
 echo
 echo ${cyn}Building Docker image ...${end}
-CONTAINER_CMD_RUN="docker build -t $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION ."
+CONTAINER_CMD_RUN="docker build --no-cache -t $CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_VERSION ."
 echo ${cyn}Running with:${end} ${grn}$CONTAINER_CMD_RUN${end}
 $CONTAINER_CMD_RUN
 echo ${cyn}Docker image built${end}
